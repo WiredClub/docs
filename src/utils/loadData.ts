f23dffffe40cd095f61b73cdfa5d47b072ee1b35
Alisson Nunes <dev.alynva@gmail.com>
2026-04-05 12:01:30 -0300

    Atualização no carregamento de dados

--- src/utils/loadData.ts
+++ src/utils/loadData.ts
@@ -4,10 +4,12 @@ import type { WiredInfoboxType } from "../content.config";
 
 export default async function loadDataByInfoboxType(type: WiredInfoboxType) {
 	const items = (await getCollection("docs"))
-		.filter(entry =>
-			entry.data.infobox
-			&& "type" in entry.data.infobox
-			&& entry.data.infobox.type === type);
+		.map(entry => {
+			if (!entry.data.infobox || !("type" in entry.data.infobox) || entry.data.infobox.type !== type)
+				return null;
+			return entry.data.infobox;
+		})
+		.filter(infobox => infobox !== null)
 
 	return items
 }
